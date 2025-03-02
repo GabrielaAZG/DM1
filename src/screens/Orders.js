c519625ddfb30ea5bd7ca6e8ce4cfa1958165f82
Alison <lrs21110028@purisima.tecnm.mx>
2025-03-02 17:44:45 -0600

    Style Orders

--- src/screens/Orders.js
+++ src/screens/Orders.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import {View, Text, TextInput, Button, SafeAreaView, Image, StyleSheet, Alert, Platform} from "react-native";
+import {View, Text, TextInput, Button, SafeAreaView, Image, StyleSheet, Alert, Platform,ImageBackground} from "react-native";
 import { OrderContext } from './OrderContext';
 
 export default function Orders({navigation}){
@@ -16,11 +16,11 @@ export default function Orders({navigation}){
         });
     }
     return(
+        <ImageBackground style={styles.backgroundImage} source={require('../../assets/4.png')}>
         <SafeAreaView>
-            <Text>ORDERS</Text>
-
+            <View style={styles.orderItem}>
             {orders.length === 0 ? (
-                <Text>There are no orders</Text>
+                <Text style={styles.text}>There are no orders</Text>
             ) : (
                 orders.map((order, index) => (
                     <View key={index} style={styles.orderItem}>
@@ -30,20 +30,35 @@ export default function Orders({navigation}){
                     </View>
                 ))
             )}
+            </View>
+
+            
 
             <Button title="EXIT" onPress={goToLogin} />
         </SafeAreaView>
+        </ImageBackground>
     )
 }
 
 
 const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // Ajusta la imagen para cubrir toda la pantalla
+        justifyContent: 'center', 
+        height: '100%'
+      },
     orderItem: {
         padding: 10,
         marginVertical: 5,
-        backgroundColor: "#f8f8f8",
+        backgroundColor: "white",
         borderRadius: 5,
+        color: 'white'
     },
+    text:{
+        color:'white',
+        fontSize:20
+    }
 });
 
 
